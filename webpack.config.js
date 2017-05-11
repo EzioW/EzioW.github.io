b6ced5da5046bc20cc726137c64c3b8c463c3110
xinzhe.yu <xinzhe.yu@wormpex.com>
2017-05-11 15:12:19 +0800

    grid pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 const config = {
@@ -12,7 +13,7 @@ const config = {
   output: {
     path: path.join(__dirname, '/Seraph/'),
     filename: '[name].[hash].bundle.js',
-    publicPath: '/',
+    publicPath: './',
   },
   module: {
     rules: [
@@ -67,6 +68,36 @@ const config = {
       title: 'Personal Website',
       template: './src/index.html',
       inject: 'body',
+    }),
+    new CleanWebpackPlugin(['Seraph']),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      comments: false,
+      compress: {
+        warnings: false,
+        drop_console: true,
+        collapse_vars: true,
+        reduce_vars: true,
+      },
+      sourceMap: true,
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({ // 抽离js中公共的部分并合并到一个文件里
+      name: 'vendor',
+      minChunks(module) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        );
+      },
+    }),
+    new webpack.optimize.CommonsChunkPlugin({ // 每次打包但是公共js部分没变的情况下不触发更新这个公共js文件
+      name: 'manifest',
+      chunks: ['vendor'],
     }),
   ],
   devServer: {
